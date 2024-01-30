e45d02821640f65149746591dfb97c8ca823c5de
harcolemas@gmail.com <harcolemas@gmail.com>
2024-01-30 20:43:02 +0500

    Added google Auth

---
+++ Frontend/intelliPrep/src/components/LoginOut.jsx
@@ -0,0 +1,17 @@
+import { GoogleLogin } from "@react-oauth/google";
+
+export default function LoginOut() {
+  return (
+    <div>
+      <GoogleLogin
+        onSuccess={(credentialResponse) => {
+          console.log(credentialResponse);
+        }}
+        onError={() => {
+          console.log("Login Failed");
+        }}
+        useOneTap
+      />
+    </div>
+  );
+}
---
+++ Frontend/intelliPrep/src/main.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import { GoogleOAuthProvider } from '@react-oauth/google';
+import './index.css'
+
+const clientID = import.meta.env.VITE_GOOGLE_CLIENT_ID;
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <GoogleOAuthProvider clientId={clientID}>
+      <App />
+    </GoogleOAuthProvider>
+  </React.StrictMode>,
+)
