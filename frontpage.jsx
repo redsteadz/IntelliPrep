cf7ce064e5746c3fd1b4464e78eca715028c54ed
Syeda Maham Mohsin <157967000+MahamMohsin@users.noreply.github.com>
2024-02-13 00:00:21 +0500

    Add files via upload

---
+++ frontpage.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import Navbar from "./Navbar";
+import "./fontpagestyles.css";
+import img from "./Animation - 1707147425041.json";
+
+function main() {
+    return <>
+     <Navbar />
+     <div className="home-container">
+        <div className="home-text">
+            <h1 className="heading">Study Smarter
+with AI insight!</h1>
+            <p className="text">
+            Meet Intelliprep :  where AI transforms your learning journey. Instant answers, personalized tips, and a virtual study partner—all in one app. Experience the future of learning with AI as your ultimate companion.
+            </p>
+            <button className="home-button">
+                Get Started
+            </button>
+        </div>
+        <div className="home-image">
+         <img src={img} alt="svg-img" />
+        </div>
+     </div>
+    </>
+};
+export default main;
